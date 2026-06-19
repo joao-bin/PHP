674a3ac755afa0b0a89ebb419ab1496f635dcf5f
lucasfilgueiras7 <lucasfilgueiras713@gmail.com>
2026-06-19 17:29:23 -0300

    Criacao de variaveis e demonstracao de tipos com exemplos aplicados em html. Close #01

---
+++ Variaveis_exemplos/variaveis_e_tipos.php
@@ -0,0 +1,20 @@
+<?php
+// String
+$nome = "Alucard";
+// INT
+$level = 20;
+// Bool
+$player = TRUE;
+//Float
+$ouro = 415.20;
+//Array
+$items = ["Espada","Escudo","Elmo"];
+
+// Os 'Prints' já vistos antes
+echo $nome;
+echo "<br>";
+echo 'Level:',$level;
+
+
+
+?>
