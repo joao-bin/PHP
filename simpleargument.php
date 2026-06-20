b28a12e28e6694b48f725752b1c4edf04d87f96a
pedro-bebop <pedrorcordeirodev@gmail.com>
2026-06-20 12:12:41 -0300

    Update simpleargument.php
    Código da função da Equação do Segundo Grau

--- simpleargument.php
+++ simpleargument.php
@@ -9,4 +9,23 @@ function hello_user( string $nome){
 
 hello_user("Pedro");
 
+ function equacaoSegundoGrau ($a, $b, $c) {
+# Declaração do caso em que o "A" não pode ser zero.
+   if ($a == 0) {
+    return " O coeficiente 'a' não pode ser zero.";
+   }
+# Calculo do Determinante (Delta).
+   $delta = ($b*$b) - (4 * $a * $c);
+# Declaração do caso em que delta nao pode ser negativo, caso sim, não existem soluções reais.
+   if ($delta < 0){
+    return "Delta é negativo, logo não existem raízes reais para esta equação.";
+   }
+# Declaração dos parâmetros que definem as raizes.
+   $x1 = (-$b +sqrt($delta)) / (2 * $a);
+   $x2 = (-$b - sqrt($delta)) / (2 * $a);
+# Retorno da Função.
+   return "Raiz x1: $x1 | Raiz x2: $x2";
+ }
+# Função chamada com os argumentos -1, -5 e 6.
+ echo equacaoSegundoGrau(1, -5, 6);
 # tag de fechamento de um script php ?> 
